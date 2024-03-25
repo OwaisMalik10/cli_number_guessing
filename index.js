7676a0c1ab7cb219fcc238017efbe46dfaa9f127
OwaisMalik10 <159901515+OwaisMalik10@users.noreply.github.com>
2024-03-25 22:44:55 +0500

    this is a first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please Guessed a Number",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulation! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number!");
+}
